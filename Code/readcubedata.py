fe8b97be5295be082adf28311e510dfbc30a781e
Saeed Mohanna <saeedmohanna@airbears2-10-142-42-168.airbears2.1918.berkeley.edu>
2019-06-28 17:37:25 -0700

    enjoy

---
+++ Code/readcubedata.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import numpy as np
+import collections
+from datetime import datetime
+import netcdf_read_write as rwr
+
+data = collections.namedtuple('data', ['filepaths', 'dates_correct', 'date_deltas',  'xvalues', 'yvalues', 'zvalues'])
+
+def reader(filepathslist):
+    """This function takes in a list of filepaths that each contain a 2d array of data, effectively taking
+    in a cube of data. It splits and stores this data in a named tuple which is returned. This can then be used
+    to extract key pieces of information."""
+    filepaths  = []
+    dates_correct , date_deltas = [], []
+    xvalues, yvalues, zvalues = [], [], []
+    for i in range(len(filepathslist)):
+        print(filepathslist[i])
+        filepaths.append(filepathslist[i])
+        datesplit = filepathslist[i].split('/')
+        date_new = datesplit[-2].replace(datesplit[-2][0:7], str(int(datesplit[-2][0:7]) + 1))
+        date_new = date_new.replace(date_new[8:15], str(int(date_new[8:15]) + 1))
+        dates_correct.append(date_new)
+        delta = abs(datetime.strptime(dates_correct[i][0:7], '%Y%j') - datetime.strptime(dates_correct[i][8:15], '%Y%j'))
+        date_deltas.append(delta.days)
+        xdata, ydata, zdata = rwr.read_grd_xyz(filepathslist[i])
+        xvalues=xdata
+        yvalues=ydata
+        zvalues.append(zdata)
+        if i == round(len(filepathslist)/2):
+            print('halfway done reading files...')
+
+    mydata = data(filepaths=np.array(filepaths), dates_correct=np.array(dates_correct), date_deltas=np.array(date_deltas), xvalues=np.array(xvalues), yvalues=np.array(yvalues), zvalues=np.array(zvalues))
+
+    return mydata
---
+++ Code/stack_corr.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import numpy as np
+import glob
+import netcdf_read_write as rwr
+import readcubedata as rcd
+
+def stack_corr(d, cutoff):
+    """This function takes in a cube of data (argument 1) and counts how many times a certain
+    piece of data is above a specified cutoff value (argument 2) in each 2-D array stored in the cube.
+    It returns a 2-D array of percentages, showing how much certain pieces of data satisfy the given cutoff
+    condition. """
+    a=np.zeros((len(d.yvalues), len(d.xvalues)))
+    i,j = 0,0
+    for z in np.nditer(d.zvalues):
+        if z >= cutoff:
+            a[i,j] = a[i,j] + 1
+        j+=1
+        if j== len(d.xvalues):
+            j=0
+            i+=1
+            if i == len(d.yvalues):
+                i=0
+    i,j = 0,0
+    for n in np.nditer(a):
+        a[i,j] = (a[i,j]/(len(d.filepaths)))*100
+        j+=1
+        if j== len(d.xvalues):
+            j=0
+            i+=1
+            if i == len(d.yvalues):
+                i=0
+        return a
+
+
+if __name__ == "__main__":
+    myfiles = glob.glob("intf_all_remote/???????_???????/corr.grd")
+    d=rcd.reader(myfiles)
+    a=stack_corr(d, 0.1)
+    rwr.produce_output_netcdf(d.xvalues, d.yvalues, a, 'Percentage', 'signalspread.nc')
+    rwr.produce_output_plot('signalspread.nc', 'Signal Spread', 'signalspread.png', 'Percentage of coherence (out of 288 images)' )
